c05faab4e79e24cdf2d2049e0bab90b4be4e37cf
Rafiur Rahman Protik <rafiurprotik111@gmail.com>
2024-06-04 22:59:50 +0600

    [DONE] public path

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const path = require('path');
-const cors = require('cors');
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
@@ -9,20 +9,25 @@ const port = 3000;
 app.use(cors());
 
 // Serve the DASH manifest file
-app.get('/video.mpd', (req, res) => {
-    console.log("called for mpd");
-    const filePath = path.join(__dirname, 'video', 'video.mpd');
-    res.sendFile(filePath);
+app.get("/video.mpd", (req, res) => {
+  console.log("called for mpd");
+  const filePath = path.join(__dirname, "video", "video.mpd");
+  res.sendFile(filePath);
 });
 
 // Serve the DASH segment files
-app.get('/video/:segment', (req, res) => {
-    console.log("called segment");
-    const segment = req.params.segment;
-    const filePath = path.join(__dirname, 'video', segment);
-    res.sendFile(filePath);
+app.get("/video/:segment", (req, res) => {
+  console.log("called segment");
+  const segment = req.params.segment;
+  const filePath = path.join(__dirname, "video", segment);
+  res.sendFile(filePath);
+});
+
+app.get("/", (req, res) => {
+  const filePath = path.join(__dirname, "public", "index.html");
+  res.sendFile(filePath);
 });
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
